1231108307f31a107b91fa7620d8e6ec1b32b88f
Oscar Daniel Perez <odprz.dev@gmail.com>
2020-08-18 10:12:29 -0500

    feat(ARQ_APP/SecurityTools): se implementa funcionalidad para realizar cifrado de textos planos

---
+++ ARQ_App/SecurityTools/PassHasher.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ARQ_App.SecurityTools
+{
+    /// <summary>
+    /// Implementa metodos para hashear y verificar una serie de caracteres
+    /// </summary>
+    public class PassHasher 
+    {
+        private const int Salt = 16;
+        private const int Key = 32;
+        private const int It = 10000; 
+
+        public PassHasher() { }
+        public string Hash(string pass)
+        {
+            using var algorithm = new Rfc2898DeriveBytes(
+                password: pass,
+                saltSize: Salt,
+                iterations: It,
+                HashAlgorithmName.SHA256);
+            var key = Convert.ToBase64String(algorithm.GetBytes(Key));
+            var salt = Convert.ToBase64String(algorithm.Salt);
+
+            return $"{It}.{salt}.{key}";
+        }
+
+        /// <summary>
+        /// Compara una serie de caracteres sin cifrar y un hash.
+        /// Verifica que al aplicar un hash el resultado sea el mismo
+        /// </summary>
+        /// <param name="hash"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public (bool Verifi, bool Up) Review(string hash, string pass)
+        {
+            var split = hash.Split('.', 3);
+
+            if(split.Length != 3)
+            {
+                throw new FormatException("El formato de cifrado es invalido ingrese un hash con el formato {iteraciones}.{salt}.{key}");
+            }
+
+            var it = Convert.ToInt32(split[0]);
+            var salt = Convert.FromBase64String(split[1]);
+            var key = Convert.FromBase64String(split[2]);
+
+            var needUp = it != It;
+
+            using var alg = new Rfc2898DeriveBytes(
+                password: pass,
+                salt: salt,
+                iterations: it,
+                HashAlgorithmName.SHA256);
+            var ktc = alg.GetBytes(Key);
+
+            var verified = ktc.SequenceEqual(key);
+
+            return (verified, needUp);
+        }
+
+
+    }
+}
